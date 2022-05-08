09cbedc3b0b366e674548527f2dcd03c18575fda
Aliyah Millán <millan_1802@csu.fullerton.edu>
2022-05-07 20:46:24 -0700

    Create HelloWorld!.cs

---
+++ C#/HelloWorld!.cs
@@ -0,0 +1,24 @@
+/*
+Aliyah Millán
+CPSC-362
+6 May 2022
+*/
+
+// C# Hello World!
+using System;
+class Program {
+  public static void Main (string[] args) {
+    Console.WriteLine ("Hello World");
+  }
+}
+
+/*
+// Hello World! program
+namespace HelloWorld {
+    class Hello {
+        static void Main(string[] args) {
+            System.Console.WriteLine("Hello World!");
+        }
+    }
+}
+*/
